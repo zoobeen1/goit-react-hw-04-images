77ebd18797bdc1f44f50206833944aa0cf21a5c2
Vlad Zubin <zoobeen1@gmail.com>
2023-10-16 22:28:40 +0300

    Update ImageLoader.jsx

--- src/components/ImageLoader/ImageLoader.jsx
+++ src/components/ImageLoader/ImageLoader.jsx
@@ -21,59 +21,57 @@ export function ImageLoader({ query, onModal }) {
     query && onLoad(query);
   }, [query]);
 
-  const onLoad = query => {
+  async function onLoad(query) {
     setStatus('pending');
     API.params.q = query;
     API.params.page = 1;
 
-    API.getPhotos()
-      .then(resp => {
-        if (resp.total === 0) {
-          toast.error(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-          setStatus('idle');
-          return;
-        }
-        toast.success(`Hooray! We found ${resp.totalHits} images.`);
-        setImgs(resp.hits);
-        setHitCounter(resp.totalHits - API.params.per_page);
-        setStatus('resolved'); //Важно чтобы было в конце - реакт перерендеривает асинхронный код на каждом шаге
-      })
-      .catch(err =>
-        console.log(
-          'Error in catch: ',
-          err.message,
-          ' response: ',
-          err.response.data
-        )
+    const resp = await API.getPhotos();
+    try {
+      if (resp.total === 0) {
+        toast.error(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        setStatus('rejected');
+        return;
+      }
+      toast.success(`Hooray! We found ${resp.totalHits} images.`);
+      setImgs(resp.hits);
+      setHitCounter(resp.totalHits - API.params.per_page);
+      setStatus('resolved'); //Важно чтобы было в конце - реакт перерендеривает асинхронный код на каждом шаге
+    } catch (err) {
+      console.log(
+        'Error in catch: ',
+        err.message,
+        ' response: ',
+        err.response.data
       );
-  };
+    }
+  }
 
-  const onLoadMore = () => {
+  async function onLoadMore() {
     API.params.page++;
 
-    API.getPhotos()
-      .then(resp => {
-        if (hitCounter < API.params.per_page) {
-          toast.info(
-            "We're sorry, but you've reached the end of search results."
-          );
+    const resp = await API.getPhotos();
+    try {
+      if (hitCounter < API.params.per_page) {
+        toast.info(
+          "We're sorry, but you've reached the end of search results."
+        );
 
-          return;
-        }
-        setImgs(prevState => [...prevState, ...resp.hits]);
-        setHitCounter(p => p - API.params.per_page);
-      })
-      .catch(err =>
-        console.log(
-          'Error in catch: ',
-          err.message,
-          ' response: ',
-          err.response.data
-        )
+        return;
+      }
+      setImgs(prevState => [...prevState, ...resp.hits]);
+      setHitCounter(p => p - API.params.per_page);
+    } catch (err) {
+      console.log(
+        'Error in catch: ',
+        err.message,
+        ' response: ',
+        err.response.data
       );
-  };
+    }
+  }
 
   // if (status === 'idle') return <div>Введите запрос</div>;
   if (status === 'pending') return <Loader />;
